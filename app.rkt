d63e7c47c96e8c29776fb56513fa6c81079752c2
Ayo Onipe <ayosemail@gmail.com>
2025-02-28 02:25:24 -0700

    Add production serve binding

--- app.rkt
+++ app.rkt
@@ -1,7 +1,26 @@
-#lang web-server/insta
+#lang racket
+ 
+(require web-server/servlet)
+(provide/contract (start (request? . -> . response?)))
 
+(require web-server/servlet-env)
+
+;; (-> request? response?)
 (define (start request)
+  (render-landing-page request))
+
+;; (-> request? response?)
+(define (render-landing-page request)
   (response/xexpr
    '(html
      (head (title "Ayo Onipe"))
      (body (h1 "Under construction")))))
+
+(serve/servlet start
+               #:launch-browser? #f
+               #:quit? #f
+               #:listen-ip #f
+               #:port 8000
+               #:servlet-path
+               "/servlets/app.rkt")
+
